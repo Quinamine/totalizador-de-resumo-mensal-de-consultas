6078382f12285e235aeecbaaa6f83baea1908ec3
Adérito Félix Quinamine <dery.felix.df@gmail.com>
2024-04-28 23:59:52 -0300

    Adicionei readme.md

--- fisiologia/menu.js
+++ fisiologia/menu.js
@@ -237,7 +237,7 @@ function eventos() {
     // PARTILHAR 
     const data = {
         title: "Totalizador de Resumo Mensal de Consultas",
-        text: "O Totalizador de Resumo Mensal de Consultas é um serviço online gratuito, que auxilia na elaboração, como o nome sugere, do resumo mensal de consultas externas, por meio do cálculo automático dos totais, com base nos dados preenchidos pelo usuário. Foi criado de acordo com o modelo da ficha de resumo mensal de consultas externas actualmente vigente no Serviço Nacional de Saúde em Moçambique.",
+        text: "O Totalizador de Resumo Mensal de Consultas é um serviço online gratuito que auxilia na elaboração, como o nome sugere, do resumo mensal de consultas externas, por meio do cálculo automático dos totais com base nos dados preenchidos pelo usuário. Foi criado de acordo com o modelo da ficha de resumo mensal de consultas externas actualmente vigente no Serviço Nacional de Saúde em Moçambique.",
         url: "https://quinamine.github.io/totalizador-de-resumo-mensal-de-consultas/index.html"
     }
 
---
+++ readme.md
@@ -0,0 +1,3 @@
+# totalizador-de-resumo-mensal-de-consultas
+
+Serviço online gratuito que auxilia na elaboração, como o nome sugere, do resumo mensal de consultas externas, por meio do cálculo automático dos totais com base nos dados preenchidos pelo usuário. Foi criado de acordo com o modelo da ficha de resumo mensal de consultas externas actualmente vigente no Serviço Nacional de Saúde em Moçambique.
