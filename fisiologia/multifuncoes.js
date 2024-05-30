d9df7c490a32ce808758a22239a451b88046fc8d
Adérito Félix Quinamine <dery.felix.df@gmail.com>
2024-05-30 01:34:58 -0300

    HTML: Envolvi as datas com a tag time. CSS: Adicionei background: greenyellow no :focus das células. JS: renomeei algumas funções para mais legibilidade (destacarCelulasComConteudoOmisso e actualizarAnoDeCopyright).

--- fisiologia/multifuncoes.js
+++ fisiologia/multifuncoes.js
@@ -30,7 +30,7 @@ function alertarSobre(msg) {
     dialogBoxDefault.classList.add("--open");
 }
 
-function destacarCelulasSaturadas() {
+function destacarCelulasComConteudoOmisso() {
     const celulas = document.querySelectorAll("[data-totalgeraleixox], [readonly]");
 
     let celulasSaturadas = 0;
@@ -72,7 +72,7 @@ const aqd = {
     }
 }
 
-function actualizarAno() {
+function actualizarAnoDeCopyright() {
     const tempo = new Date();
     let anoActual = tempo.getFullYear();
 
@@ -117,8 +117,8 @@ window.addEventListener("load", () => {
     }));
 
     const inputsCelulares = document.querySelectorAll("[data-totalgeraleixox]");
-    inputsCelulares.forEach (inputCelular => inputCelular.addEventListener("input", destacarCelulasSaturadas));
-    destacarCelulasSaturadas();
+    inputsCelulares.forEach (inputCelular => inputCelular.addEventListener("input", destacarCelulasComConteudoOmisso));
+    destacarCelulasComConteudoOmisso();
 
     
     aqd.mostrarAviso();
@@ -126,7 +126,7 @@ window.addEventListener("load", () => {
     dialogBoxAQD__btn.addEventListener("click", aqd.salvarCiencia);
 
     // Actualizar o ano 
-    actualizarAno();
+    actualizarAnoDeCopyright();
     sugerirMesActual();
     formatarNumeros();
 
