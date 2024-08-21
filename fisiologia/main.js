7b5943b05a253b21159925ec1c41b8f459ac5487
Adérito Félix Quinamine <quinamine.aderitofelix@gmail.com>
2024-08-21 20:35:45 -0300

    HTML/CSS/JS: Minifiquei todos os arquivos tirando os espaços entre os blocos e entre as linhas dentro de alguns blocos.

--- fisiologia/main.js
+++ fisiologia/main.js
@@ -1,19 +1,14 @@
 "use strict"
-
 const backup = {
     saveGridInputs() {
         const inputsCelulares = document.querySelectorAll("[data-totalgeraleixox]");
-
         for (let i = 0; i < inputsCelulares.length; i++) {
-            
             inputsCelulares[i].addEventListener("input", () => {
                 localStorage.setItem(`${keyPrefix}-input${i}`, inputsCelulares[i].value);
             });
             inputsCelulares[i].value = localStorage.getItem(`${keyPrefix}-input${i}`);
         }
-        
     },
-    
     saveExtraInputs() {
         const inputsNaoCelulares = document.querySelectorAll(".input-nao-celular");
         inputsNaoCelulares.forEach( extraInput => {
@@ -22,70 +17,55 @@ const backup = {
         });
     }
 }
-
 const totalizador = {
     filtrarEtotalizarCelulas(inputTarget) {
         // Subtotal eixo x
         let classNameDosOperandos = inputTarget.dataset.subtotaleixox;
         inputTarget.classList.add(`${classNameDosOperandos}`);
-
         let operandos = document.querySelectorAll(`.${classNameDosOperandos}`);
         let celulaDeSaida = document.querySelector(`.${inputTarget.dataset.subtotaleixoxoutput}`);
         celulaDeSaida.value = this.somar(operandos);
-
         // Total parcial
         classNameDosOperandos = inputTarget.dataset.totalparcialeixox;
         inputTarget.classList.add(`${classNameDosOperandos}`);
-
         operandos = document.querySelectorAll(`.${classNameDosOperandos}`);
         celulaDeSaida = document.querySelector(`.${inputTarget.dataset.totalparcialeixoxoutput}`);
         celulaDeSaida.value = this.somar(operandos);
-        
         // Total geral
         classNameDosOperandos = inputTarget.dataset.totalgeraleixox;
         inputTarget.classList.add(`${classNameDosOperandos}`);
-
         operandos = document.querySelectorAll(`.${classNameDosOperandos}`);
         celulaDeSaida = document.querySelector(`.${inputTarget.dataset.totalgeraleixoxoutput}`);
         celulaDeSaida.value = this.somar(operandos);
-
         if(inputTarget.dataset.subtotaleixoy) {
             classNameDosOperandos = inputTarget.dataset.subtotaleixoy;
             inputTarget.classList.add(`${classNameDosOperandos}`);
-
             operandos = document.querySelectorAll(`.${classNameDosOperandos}`);
             celulaDeSaida = document.querySelector(`.${inputTarget.dataset.subtotaleixoyoutput}`);
             celulaDeSaida.value = this.somar(operandos);
         }
-
         if(inputTarget.dataset.totaleixoy) {
             classNameDosOperandos = inputTarget.dataset.totaleixoy;
             inputTarget.classList.add(`${classNameDosOperandos}`);
-
             operandos = document.querySelectorAll(`.${classNameDosOperandos}`);
             celulaDeSaida = document.querySelector(`.${inputTarget.dataset.totaleixoyoutput}`);
             celulaDeSaida.value = this.somar(operandos);
         }
-    
         if(inputTarget.dataset.totalparcialeixoy) {
             classNameDosOperandos = inputTarget.dataset.totalparcialeixoy;
             inputTarget.classList.add(`${classNameDosOperandos}`);
-
             operandos = document.querySelectorAll(`.${classNameDosOperandos}`);
             celulaDeSaida = document.querySelector(`.${inputTarget.dataset.totalparcialeixoyoutput}`);
             celulaDeSaida.value = this.somar(operandos);
         }
-    
         if(inputTarget.dataset.totalgeraleixoy) {
             classNameDosOperandos = inputTarget.dataset.totalgeraleixoy;
             inputTarget.classList.add(`${classNameDosOperandos}`);
-
             operandos = document.querySelectorAll(`.${classNameDosOperandos}`);
             celulaDeSaida = document.querySelector(`.${inputTarget.dataset.totalgeraleixoyoutput}`);
             celulaDeSaida.value = this.somar(operandos);
         }
     },
-    
     somar(celulasPorTotalizar) {
         let soma = 0;
         for(const c of celulasPorTotalizar) {
@@ -94,8 +74,6 @@ const totalizador = {
         return soma;
     }
 }
-
-
 function escutarEventos() {
     const inputsCelulares = document.querySelectorAll("[data-totalgeraleixox]");
     inputsCelulares.forEach( inputCelular => {
@@ -103,7 +81,6 @@ function escutarEventos() {
         inputCelular.value !== "" && totalizador.filtrarEtotalizarCelulas(inputCelular);
     });
 }
-
 window.addEventListener("load", () => {
     backup.saveGridInputs();
     backup.saveExtraInputs();
